f46464966639b1c409130e262dea1c1623062736
Unknown <a.m.vivas.rodriguez@st.hanze.nl>
2018-05-29 17:24:24 +0200

    Added textInterface.sh

---
+++ textInterface.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#==================================================
+# testing a text interface for Linux - Week 2
+#
+#
+#==================================================
+echo "============================="
+echo " Rodriguez's Backup Tool"
+echo "============================="
+echo -e "Please specify the folder to copy pictures to (Month|Week): \c"
+read word
+echo ""
+echo "---------------------------------------"
+echo "The chosen directory was: $word"
+echo "---------------------------------------"
+echo -e "Is this correct?[y/n]: \c"
+read answer
+echo ""
+echo "Progress:"
+echo "--------------"
+
+if [[ $answer = "y" ]]
+then
+  echo "process 1................"
+  echo "process 2................"
+elif [[ $answer = "n" ]]
+then
+  echo "process Terminated.............."
+else
+  echo "!Error!: Invalid option, $answer"
+fi
+
+
+
